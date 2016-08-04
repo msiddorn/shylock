0ed8c22e0ded393853b14fcbc5c0b5b7920c6815
Mark Siddorn <msiddorn@gmail.com>
2016-08-04 23:10:41 +0100

    system test

--- run_system_tests.sh
+++ run_system_tests.sh
@@ -1,3 +1,10 @@
+# start local server
+python3 app.py 9555 &
+server_pid=$!
+
+# always kill server
+trap 'kill $server_pid' EXIT SIGINT
+
 for file in tests/system/test_*.py; do
     py.test "$file"
 done
--- tests/system/test_api.py
+++ tests/system/test_api.py
@@ -1,86 +1,114 @@
+'''
+    System test for the webapi commands
+    assumes server is running locally on port 9555 (make this an argument at some point)
+    pytest order is made explicitly
+'''
 import pytest
 import requests
-# import json
+import json
+import re
 
 
-address_prefix = 'https://calm-brushlands-26293.herokuapp.com/v1/'
+address_prefix = 'http://0.0.0.0:9555/v1/'
+json_header = {'Content-Type': 'application/json'}
 
 
 @pytest.mark.order1
-def test_home_page(server):
-    r = requests.post(address_prefix[:-3])
-    assert(r.text == 'bob')
+def test_home_page():
+    r = requests.get(address_prefix[:-3])
+    print(r.text)
+    assert(json.loads(r.text) == 'hello world')
 
-'''
-if argv[1] == 'local':
-elif argv[1] == 'heroku':
-    address_prefix = 'https://calm-brushlands-26293.herokuapp.com/v1/'
+@pytest.mark.order2
+def test_pools():
+    results = []
+    for pool_name in ['Paris', 'Rome']:
+        results.append(requests.post(
+            address_prefix + 'pools',
+            data=json.dumps({'name': pool_name}),
+            headers=json_header,
+        ))
+    assert(results[0].status_code == 200)
+    assert(results[1].status_code == 200)
 
+    pools = json.loads(requests.get(address_prefix + 'pools').text)
+    assert(pools['0']['name'] == 'Paris')
+    assert(pools['1']['name'] == 'Rome')
 
-api_calls = [
-    ('add new pool for paris trip', 'pools', {'name': 'Paris'}),
-    ('add new pool for rome trip', 'pools', {'name': 'Rome'}),
-    ('list pools', 'pools', None),
-    ('Add Mark to Paris trip', '0/users', {'name': 'Mark'}),
-    ('Add Sophie to Paris trip', '0/users', {'name': 'Sophie'}),
-    ('Add Tom to Paris trip', '0/users', {'name': 'Tom'}),
-    ('Try to add Tom to Paris trip again', '0/users', {'name': 'Tom'}),
-    ('List users for Paris', '0/users', None),
-    ('List users for Rome', '1/users', None),
-    ('List users for Timbuktu', '2/users', None),
-    (
-        'Mark spends £30 on everyone',
-        '0/transactions',
-        {
+@pytest.mark.order3
+def test_users():
+    results = []
+    for user_name in ['Mark', 'Sophie', 'Tom', 'Tom']:
+        results.append(requests.post(
+            address_prefix + '0/users',
+            data=json.dumps({'name': user_name}),
+            headers=json_header,
+        ))
+    results.append(requests.post(
+        address_prefix + '99/users',
+        data=json.dumps({'name': 'A person'}),
+        headers=json_header,
+    ))
+    results.append(requests.get(address_prefix + '0/users'))
+    for r in results[:3]:
+        assert(r.status_code == 200)
+    assert(results[3].status_code == 403)
+    assert(results[4].status_code == 404)
+
+    paris_users, rome_users, timbuktu_users = (
+        requests.get(address_prefix + str(pool) + '/users')
+        for pool in range(3)
+    )
+    assert(json.loads(paris_users.text) == ['Mark', 'Sophie', 'Tom'])
+    assert(json.loads(rome_users.text) == [])
+    assert(timbuktu_users.status_code == 404)
+
+@pytest.mark.order4
+def test_transactions():
+    transactions = [
+        ('0', {
             'spender': 'Mark',
             'amount': 30,
-            'consumers': {
-                'Mark': 1,
-                'Sophie': 1,
-                'Tom': 1,
-            },
-        }
-    ),
-    (
-        'Tom buys sophie an ice cream and Mark a double cone',
-        '0/transactions',
-        {
+            'consumers': {'Mark': 1, 'Sophie': 1, 'Tom': 1},
+        }),
+        ('0', {
             'spender': 'Tom',
             'amount': 7.5,
-            'consumers': {
-                'Mark': 2,
-                'Sophie': 1,
-            },
-        }
-    ),
-    (
-        'Mark tries to spend money for a pool he\'s not in',
-        '1/transactions',
-        {
+            'consumers': {'Mark': 2, 'Sophie': 1},
+        }),
+        ('1', {
             'spender': 'Mark',
             'amount': 1,
-            'consumers': {
-                'Mark': 1,
-                'Sophie': 1,
-            },
-        }
-    ),
-    ('List the balances for the Paris trip', '0/balances', None),
-    ('List the balances for the Rome trip', '1/balances', None),
-    ('List all of the old transactions for the Paris trip', '0/transactions', None),
-]
+            'consumers': {'Mark': 1, 'Sophie': 1, 'Tom': 1},
+        }),
+        ('0', {
+            'spender': 'Mark',
+            'consumers': {'Mark': 1, 'Sophie': 1, 'Tom': 1},
+        }),
+    ]
+    results = [
+        requests.post(
+            address_prefix + pool + '/transactions',
+            data=json.dumps(transaction),
+            headers=json_header,
+        )
+        for pool, transaction in transactions
+    ]
+    assert(results[0].status_code == 200)
+    assert(results[1].status_code == 200)
+    assert(results[2].status_code == 404)
+    assert(results[3].status_code == 400)
 
-headers = {'Content-Type': 'application/json'}
+    seen_transactions = json.loads(requests.get(address_prefix + '0/transactions').text)
+    for i, transaction in enumerate(seen_transactions):
+        match = re.match('(\w+) spent ([0-9\.]+) on (\{.*\})', transaction)
+        assert(match.groups()[0] == transactions[i][1]['spender'])
+        assert(match.groups()[1] == str(transactions[i][1]['amount']))
+        # Don't bother parsing consumers, the system tests are supposed to be just for the api
 
-for description, api_call, data in api_calls:
-    print(description)
-    address = address_prefix + api_call
-    if data is None:
-        print('Calling get on "{}"'.format(address))
-        r = requests.get(address)
-    else:
-        print('Calling post on "{}" with {}'.format(address, data))
-        r = requests.post(address, data=json.dumps(data), headers=headers)
-
-    print('Response was:\n{}\n'.format(r.text))
-'''
+@pytest.mark.order5
+def test_balances():
+    balances = json.loads(requests.get(address_prefix + '0/balances').text)
+    assert(balances['Mark'] == -15)
+    assert(balances['Sophie'] == 12.5)
+    assert(balances['Tom'] == 2.5)
